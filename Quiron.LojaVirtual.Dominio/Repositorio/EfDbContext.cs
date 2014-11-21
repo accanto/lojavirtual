17945086a5e92cc8d13297960ace78c76f12217e
accanto <cmcoelho@accanto.com.br>
2014-11-21 16:06:05 -0200

    Entity

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext: DbContext
+    {
+        // remove a pluralização e define o nome da tabela no BD
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
